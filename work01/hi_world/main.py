fb936cfcb9400c3e830f188058acdb91fcd9a449
Glebuc <gleb567892@gmail.com>
2023-09-23 23:06:28 +0300

    fix fetch

--- work01/hi_world/main.py
+++ work01/hi_world/main.py
@@ -1,32 +1,21 @@
-import turtle
-import math
+import tkinter as tk
+import random
 
-screen = turtle.Screen()
-screen.title('5 Degree Square Spiral in a Square - PythonTurtle.Academy')
-screen.setup(1000,1000)
-screen.setworldcoordinates(-1000,-1000,1000,1000)
-turtle.speed(0)
-turtle.hideturtle()
+window = tk.Tk()
+window.title("Генератор паролей")
+window["bg"] = "white"
+window.geometry("350x200")
+window.resizable(False,False)
 
-def draw_square(x,y,direction,length):
-    turtle.up()
-    turtle.goto(x,y)
-    turtle.seth(direction)
-    turtle.back(length/2)
-    turtle.left(90)
-    turtle.back(length/2)
-    turtle.seth(direction)
-    turtle.down()
-    for _ in range(4):
-        turtle.fd(length)
-        turtle.left(90)
-
-def square_spiral(x,y,direction,length):
-    if length < 5: return
-    
-    draw_square(x,y,direction,length)
-    square_spiral(x,y,direction+alpha,length/(math.sin(math.radians(alpha)) + math.cos(math.radians(alpha))))
+lbl = tk.Label(text="Создай пароль", bg="white", font=16, fg="purple")
+lbl.pack()
+lbl2 = tk.Label(text="Введите количество символов в пароле", bg="white", font=("Arial", 12), fg="purple")
+lbl2.pack()
+inp = tk.Entry(font=13)
+inp.pack()
+btn = tk.Button(text="Сгенерировать", font=("Arial", 12), bg="purple", fg="white")
+btn.pack(pady=20)
 
 
-alpha=5
-square_spiral(0,0,0,1600)
+
+window.mainloop()
